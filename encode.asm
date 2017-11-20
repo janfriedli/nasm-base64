1b0d0cd10fc0a408298e8afcce4f7fbdda37d304
Jan Friedli <jan.friedli@gmx.ch>
2017-11-20 07:58:51 +0100

    mark first 6 bits

--- encode.asm
+++ encode.asm
@@ -10,7 +10,7 @@
 ;
 ;  Build using these commands:
 ;    nasm -f elf64 -g -F stabs encode.asm
-;    ld -o encode encode.o
+;    gcc -o encode encode.o
 ;
 
 SECTION .bss										; Section containing uninitialized data
@@ -22,91 +22,25 @@ SECTION .data										; Section containing initialised data
 
 	; this map is used to get the base64 representation of the coresponding 6 bits.
 	base64Charactermap:	db "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-	DumpLin:	db " 00 00 00 00 "
-	DUMPLEN		EQU $-DumpLin
-	FULLLEN		EQU $-DumpLin
-	formatout: db "%ld", 0 ; newline, nul terminator
+	testString: db "testing" ; should become dGVzdGluZw==
+	formatout: db "%d", 0 ; newline, nul terminator
+	testMsg: db "The quieter we become the more we are able to hear",10,0
 
 
 SECTION .text			; Section containing code
 
-;-------------------------------------------------------------------------
-; PrintLine: 	Displays DumpLin to stdout
-; UPDATED: 	4/15/2009
-; IN: 		Nothing
-; RETURNS:	Nothing
-; MODIFIES: 	Nothing
-; CALLS:	Kernel sys_write
-; DESCRIPTION:	The hex dump line string DumpLin is displayed to stdout
-; 		using INT 80h sys_write. All GP registers are preserved.
-
-PrintLine:
-	push rax		  ; Save all used registers
-	push rbx		  ; Save all used registers
-	push rcx		  ; Save all used registers
-	push rdx		  ; Save all used registers
-	mov eax,4	  ; Specify sys_write call
-	mov ebx,1	  ; Specify File Descriptor 1: Standard output
-	mov ecx,DumpLin	  ; Pass offset of line string
-	mov edx,FULLLEN	  ; Pass size of the line string
-	int 80h		  ; Make kernel call to display line string
-	pop rdx		  ; Restore all caller's registers
-	pop rcx		  ;
-	pop rbx		  ;
-	pop rax		  ;
-	ret		  ; Return to caller
-
-;-------------------------------------------------------------------------
-; LoadBuff: 	Fills a buffer with data from stdin via INT 80h sys_read
-; UPDATED: 	4/15/2009
-; IN: 		Nothing
-; RETURNS:	# of bytes read in EBP
-; MODIFIES: 	ECX, EBP, Buff
-; CALLS:	Kernel sys_write
-; DESCRIPTION:	Loads a buffer full of data (BUFFLEN bytes) from stdin
-;		using INT 80h sys_read and places it in Buff. Buffer
-;		offset counter ECX is zeroed, because we're starting in
-;		on a new buffer full of data. Caller must test value in
-;		EBP: If EBP contains zero on return, we hit EOF on stdin.
-;		Less than 0 in EBP on return indicates some kind of error.
-
-LoadBuff:
-	push rax	  ; Save caller's EAX
-	push rbx	  ; Save caller's EBX
-	push rdx	  ; Save caller's EDXh
-	mov eax,3	  ; Specify sys_read call
-	mov ebx,0	  ; Specify File Descriptor 0: Standard Input
-	mov ecx,Buff	  ; Pass offset of the buffer to read to
-	mov edx,BUFFERLENGTH	  ; Pass number of bytes to read at one pass
-	int 80h		  ; Call sys_read to fill the buffer
-	mov ebp,eax	  ; Save # of bytes read from file for later
-	xor ecx,ecx	  ; Clear buffer pointer ECX to 0
-	pop rdx		  ; Restore caller's EDX
-	pop rbx		  ; Restore caller's EBX
-	pop rax		  ; Restore caller's EAX
-	ret		  ; And return to caller
-
 extern scanf, printf
 
 global main
-; ------------------------------------------------------------------------
-; MAIN PROGRAM BEGINS HERE
-;-------------------------------------------------------------------------
+
 main:
 	nop			; No-ops for GDB
 	nop
 
-	call LoadBuff
-	mov eax, Buff
-
-	convert:
-	    xor dl, dl              ; start with 0
-	    shr eax, 1              ; shift off a bit (into CF)
-	    adc dl, 48              ; add '0', plus CF -- yields '0' or '1'
-	    mov [ebx+ecx-1], dl     ; set from the end
-	    loop convert
-
-
+	xor eax, eax
+	mov eax, 122
+	mov ebx, 63 ; mark 6 bits
+ 	and ebx, eax
 
 Exit:	mov eax,1		; Code for Exit Syscall
 	mov ebx,0		; Return a code of zero
