7cc7d1028866695f69735dffd137011c8796bbd3
Jan Friedli <jan.friedli@gmx.ch>
2017-12-07 19:02:25 +0100

    remove unnecessary code and reformat

--- encode.asm
+++ encode.asm
@@ -7,24 +7,23 @@
 ;  Date modification: 14/11/2017
 ;  Description     : A base64 encoder
 
-SECTION .bss										; Section containing uninitialized data
+SECTION .bss				; Section containing uninitialized data
 
 	BUFFERLENGTH EQU 3						; reserve 3 bytes which will be processed ad 6bit * 4
 	Buff	resb BUFFERLENGTH
-	var1: resb 64
 	res: resb 64
-SECTION .data										; Section containing initialised data
+
+SECTION .data				; Section containing initialised data
 
 	; this map is used to get the base64 representation of the coresponding 6 bits.
 	base64Charactermap:	db "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-	inputMsg: db "Please enter anything you want to encode in base64: ",0
+	inputMsg: db "Please enter anything you want to encode in base64: ", 10, 0
 	inputType: db "%s",0
-	formatTypeOut: db "Encoded value is: %s", 10, 0
-	formatGreet:    db "%s", 0	; The printf format
-
-
+	formatTypeOut: db "%s", 10, 0
+	formatGreet:    db "%s", 0		; The printf format
 
 SECTION .text			; Section containing code
+
 	extern scanf, printf
 	global main
 
@@ -39,39 +38,40 @@ main:
 	call printf
 
 	Read:
-		mov eax,3		; Specify sys_read call
-		mov ebx,0		; Specify File Descriptor 0: Standard Input
-		mov ecx,Buff		; Pass offset of the buffer to read to
-		mov edx,BUFFERLENGTH		; Pass number of bytes to read at one pass
+		; Read the necessary data block
+		mov eax, 3		; Specify sys_read call
+		mov ebx, 0		; Specify File Descriptor 0: Standard Input
+		mov ecx, Buff		; Pass offset of the buffer to read to
+		mov edx, BUFFERLENGTH		; Pass number of bytes to read at one pass
 		int 80h			; Call sys_read to fill the buffer
-		mov ebp,eax		; Save # of bytes read from file for later
-		cmp eax,0		; If eax=0, sys_read reached EOF on stdin
+		mov ebp, eax		; Save # of bytes read from file for later
+		cmp eax, 0		; If eax=0, sys_read reached EOF on stdin
 		je Done			; Jump If Equal (to 0, from compare)
 
-	Scan:
-		;; Some cleaning
+		; Some cleaning
 		xor rax, rax
 		xor rsi, rsi
 		xor rdi, rdi
 		xor rbx, rbx
 		xor rcx, rcx
 		xor rdx, rdx
+		xor [res], dword res
 
-		;; Fill the data for further processing
+		; Fill the data for further processing
 		mov byte bh, [Buff]
 		mov byte bl, [Buff+1]
 		shl rbx, 16
 		mov byte bh, [Buff+2]
 
-		;; First 6 Bits
+		; First 6 Bits
 		mov rdx, rbx
-		shl rbx, 38 		;Move the finished 6 Bits away
-		shr rbx, 32		;Sets the bits back to the right place
+		shl rbx, 38 		; Move the finished 6 Bits away
+		shr rbx, 32		; Sets the bits back to the right place
 		shr rdx, 26
 		mov byte al, [base64Charactermap+rdx]
 		mov [res], al
 
-		;; Next 6 Bits
+		; Next 6 Bits
 		mov rdx, rbx
 		shl rbx, 38
 		shr rbx, 32
@@ -79,7 +79,7 @@ main:
 		mov byte al, [base64Charactermap+rdx]
 		mov [res+1], al
 
-		;; NeXT 6 Bits
+		; NeXT 6 Bits
 		mov rdx, rbx
 		shl rbx, 38
 		shr rbx, 32
@@ -87,7 +87,7 @@ main:
 		mov byte al, [base64Charactermap+rdx]
 		mov [res+2], al
 
-		;; Last 6 bits
+		; Last 6 bits
 		mov rdx, rbx
 		shl rbx, 38
 		shr rbx, 32
@@ -95,17 +95,15 @@ main:
 		mov byte al, [base64Charactermap+rdx]
 		mov [res+3], al
 
-		;print the result
+		; print the result
 		mov rsi, res
-		mov rdi, formatTypeOut	;Formattype for printf
+		mov rdi, formatTypeOut	; Formattype for printf
 		xor rax , rax
 		call printf
 
-		; Bump the buffer pointer to the next character and see if we're done:
+		; Bump the buffer pointer
 		inc ecx		; Increment line string pointer
-		cmp ecx,ebp	; Compare to the number of characters in the buffer
-		jna Scan	; Loop back if ecx is <= number of chars in buffer
-		jmp Read
+		jmp Read	; jump back and repeat untill we have nothing left
 
 
 	; All done! Let's end this party:
