faa9efa7d11c92af139b80b2526c4d93fb803d49
dominikmeister <dominik.meister@hotmail.ch>
2017-12-07 10:13:01 +0100

    1

--- encode.asm
+++ encode.asm
@@ -17,8 +17,8 @@ SECTION .bss										; Section containing uninitialized data
 
 	BUFFERLENGTH EQU 6						; reserve 6 bytes for each process step
 	Buff	resb BUFFERLENGTH
-	var1: resb 16
-
+	var1: resb 64
+	res: resb 64
 SECTION .data										; Section containing initialised data
 
 	; this map is used to get the base64 representation of the coresponding 6 bits.
@@ -50,8 +50,55 @@ main:
 	mov rsi, var1	;Address of returnvalue
 	call scanf ; read input that will be base46 encoded
 
+	;; Some cleaning 
+	xor rax, rax
+	xor rsi, rsi
+	xor rdi, rdi
+	xor rbx, rbx
+	xor rcx, rcx
+	xor rdx, rdx
+
+	;; Fill the data for further processing
+	mov byte bh, [var1]
+	mov byte bl, [var1+1]
+	shl rbx, 16
+	mov byte bh, [var1+2]
+
+	;; First 6 Bits
+	mov rdx, rbx
+	shl rbx, 38 		;Move the finished 6 Bits away
+	shr rbx, 32		;Sets the bits back to the right place
+	shr rdx, 26
+	mov byte al, [base64Charactermap+rdx]
+	mov [res], al
+
+	;; Next 6 Bits
+	mov rdx, rbx
+	shl rbx, 38
+	shr rbx, 32
+	shr rdx, 26
+	mov byte al, [base64Charactermap+rdx]
+	mov [res+1], al
+
+	;; NeXT 6 Bits
+	mov rdx, rbx
+	shl rbx, 38
+	shr rbx, 32
+	shr rdx, 26
+	mov byte al, [base64Charactermap+rdx]
+	mov [res+2], al
+
+	;; Last 6 bits
+	mov rdx, rbx
+	shl rbx, 38
+	shr rbx, 32
+	shr rdx, 26
+	mov byte al, [base64Charactermap+rdx]
+	mov [res+3], al
+	
+	
 	;print the result
-	mov rsi, var1
+	mov rsi, res
 	mov rdi, formatTypeOut	;Formattype for printf
 	xor rax , rax
 	call printf
