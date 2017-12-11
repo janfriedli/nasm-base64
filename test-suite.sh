3b0a7551fb08b1578b431e11087754858c18777f
Jan Friedli <jan.friedli@gmx.ch>
2017-12-11 18:07:40 +0100

    added testuite

---
+++ test-suite.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+make
+
+# test single loop with =
+if [ "$(echo 'sa' | ./encode)" == "c2E=" ]; then
+  echo -e "\e[92m single loop with = passed"
+else
+  echo -e "\033[31m single loop with = FAILED"
+  exit 1
+fi
+
+# test single loop with ==
+if [ "$(echo '0' | ./encode)" == "MA==" ]; then
+  echo -e "\e[92m single loop with == passed"
+else
+  echo -e "\033[31m single loop with == FAILED"
+  exit 1
+fi
+
+# test single loop no =
+if [ "$(echo 'sss' | ./encode)" == "c3Nz" ]; then
+  echo -e "\e[92m single loop no = passed"
+else
+  echo -e "\033[31m single loop no = FAILED"
+fi
+
+# test double loop with =
+if [ "$(echo 'sasaa' | ./encode)" == "c2FzYWE=" ]; then
+  echo -e "\e[92m double loop with = passed"
+else
+  echo -e "\033[31m double loop with = FAILED"
+  exit 1
+fi
+
+# test double loop no =
+if [ "$(echo 'sasaas' | ./encode)" == "c2FzYWFz" ]; then
+  echo -e "\e[92m double loop no = passed"
+else
+  echo -e "\033[31m double loop no = FAILED"
+  exit 1
+fi
+
+# test double loop with ==
+if [ "$(echo 'sasaass' | ./encode)" == "c2FzYWFzcw==" ]; then
+  echo -e "\e[92m double loop with == passed"
+else
+  echo -e "\033[31m double loop with == FAILED"
+  exit 1
+fi
+
+exit 1
